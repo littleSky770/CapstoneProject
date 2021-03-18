d2870006d4a542e84a6391159eae2489170a589f
Alex <paigedaughters@my.unt.edu>
2021-03-18 07:25:30 -0500

    addes html pages for the Safety Analysis Tool page buttons. Also commented the user out of each of the html files because it was silly to put it in every file just because the login and register pages didnt need it.

--- proj/app.py
+++ proj/app.py
@@ -13,6 +13,8 @@ app.secret_key = 'this is a key'
 #app.permanent_session_lifetime = timedelta(minutes=5)
 
 
+#################################### Database Functions ################################################
+
 # Create database for user accounts and anything else
 con = sqlite3.connect('data.db', check_same_thread=False, timeout=10000)
 
@@ -47,6 +49,7 @@ con.commit()
 #        cur.execute("UPDATE Users SET unit_num = (?) WHERE email = (?)", (unit_user, user))
 #        con.commit()
 
+########################################## Home ########################################################
 #hardcoded for now
 #name will be stored by the session and passed to the pages
 name = 'Guest'
@@ -57,6 +60,8 @@ def home():
 
     return render_template('index.html', usr = name)
 
+
+######################################## Login and Resister ###########################################
 #login page the POST and GET methods are made clear here because we will be sending data
 @app.route("/login/", methods=["POST", "GET"])
 def login():
@@ -80,12 +85,16 @@ def register():
     else:
         return render_template('register.html')
 
+
+######################################## Details of Systems ##############################################
 #Details of Systems main page
 @app.route("/detailsSys/")
 def detailsSys():
 
     return render_template('detailsSys.html', usr = name)
 
+
+######################################## Safety Related Data #############################################
 #Safety Related Data main page
 @app.route("/safetyRelatedData/")
 def safetyRelatedData():
@@ -110,11 +119,31 @@ def saveDetailsToDB():
 
     return render_template('saveDetailsToDB.html', usr = name)
 
+
+######################################## Safety Analysis Tool ############################################
 #Safety Analysis Tool main page
 @app.route("/safetyAnalysisTool/")
 def safetyAnalysisTool():
 
     return render_template('safetyAnalysisTool.html', usr = name)
+
+@app.route("/safetyAnalysisTool/uploadExistingSys/")
+def uploadExistingSys():
+
+    return render_template('uploadExistingSys.html', usr = name)
+
+@app.route("/safetyAnalysisTool/addNewSys/")
+def addNewSys():
+
+    return render_template('addNewSys.html', usr = name)
+
+@app.route("/safetyAnalysisTool/loadFromDB/")
+def loadFromDB():
+
+    return render_template('loadFromDB.html', usr = name)
+
+
+############################################ End ########################################################
 
 if __name__ == '__main__':
     app.run(debug=True)
